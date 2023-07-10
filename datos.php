16e8fe1dfbf817780d87ba0ad2732d676e24403c
Adrian Soto <xasg@hotmail.com>
2023-07-10 14:54:21 -0600

    Cambios realizados al registro por solicitud del usuario, cambios en imagenes de portada sobre el mapa y las regiones de paticipación, cambios realizados en el registro

--- datos.php
+++ datos.php
@@ -2,9 +2,11 @@
    include_once('databases_registro.php');
    session_start();
    mysqli_set_charset( $mysqli, 'utf8');  
-   $id= $_SESSION["id"];
-  
+   $id= $_SESSION["id"];  
    $participante = run_participante($id);
+   $region = $participante['dt_region'];
+   $cede = $participante['dt_cede'];
+
    ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -62,50 +64,23 @@
          <img class="img-fluid" src="img/cintillo_header.png" width="100%" style="margin-top: 2%;">
       </div>
 
-
-
-
         <div class="col-sm-8 col-md-8 offset-md-2 text-center"><br><br>
-                              <h4 class="mb-0">HOLA <?php echo $participante['dt_nombre']," ".$participante['dt_apaterno']." ".$participante['dt_amaterno']; ?> TU REGISTRO SE REALIZÓ CORRECTAMENTE</h4><br>   
+                              <h4 class="mb-0">Hola <?php echo $participante['dt_nombre']," ".$participante['dt_apaterno']." ".$participante['dt_amaterno']; ?> tu registro se realizó correctamente</h4><br>   
                           
-                              <h5 class="mb-0">Su numero de folio es: <strong><?php echo $participante['id_usuario'];?> </strong></h5><br>
-                              <?php    
-                                 $region = $participante['dt_region'];
-                                 $region = 5;
+                              <h4 class="mb-0">Su numero de folio es: <strong><?php echo $participante['id_usuario'];?> </strong></h4><br>
 
-                                 if ($region == 1)
-                                 {
-                                    echo "<h5 class='mb-0'>La sede del evento sera en TecNM de Tuxtla Gutierrez <strong> </strong></h5><br>";      
-                                    echo "'<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.400951184565!2d-93.17515092795986!3d16.756715984026815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd964bb4d07fd%3A0x4e771d3242c2f25a!2sTecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20Campus%20Tuxtla%20Guti%C3%A9rrez!5e0!3m2!1ses-419!2smx!4v1688681077598!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                 }
-                                else if ($region == 2)
-                                {
-                                    echo "<h5 class='mb-0'>La sede del evento sera en la BUAP Complejo Cultural <strong> </strong></h5><br>";      
-                                    echo "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15087.971620470136!2d-98.25810024458008!3d19.0200343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc742407551ef%3A0x93dc3db3e62b8694!2sComplejo%20Cultural%20Universitario%20BUAP!5e0!3m2!1ses-419!2smx!4v1688682100804!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                }
-                                else if ($region == 3)
-                                {
-                                 echo "<h5 class='mb-0'>La sede del evento sera en la Universidad Tecnológica de Morelia <strong> </strong></h5><br>";      
-                                 echo "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30045.81552628686!2d-101.19562367514412!3d19.72424622352978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842d0e2765411ef1%3A0x3ca21a3f9f26b9b1!2sUniversidad%20Tecnol%C3%B3gica%20de%20Morelia!5e0!3m2!1ses-419!2smx!4v1688683430036!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                }
-                                else if ($region == 4)
-                                {
-                                 echo "<h5 class='mb-0'>La sede del evento sera en la Universidad Politecnica de San Luis Potosi <strong> </strong></h5><br>";      
-                                 echo "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7392.190818910448!2d-100.98803124284262!3d22.1223377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842a9f5412c6b56f%3A0xe1b3dea4c6aa1c03!2sUniversidad%20Polit%C3%A9cnica%20de%20San%20Luis%20Potos%C3%AD!5e0!3m2!1ses-419!2smx!4v1688683677930!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                }
-                                else if ($region == 5)
-                                {
-                                 echo "<h5 class='mb-0'>La sede del evento sera en la Universidad Tecnológica de Guaymas <strong> </strong></h5><br>";      
-                                 echo "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3526.231733698687!2d-110.89561842739703!3d27.894861176075505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86c970a075d21e87%3A0xbdbcabdc4ed64e81!2sInstituto%20Tecnol%C3%B3gico%20de%20Guaymas!5e0!3m2!1ses-419!2smx!4v1688683907911!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                }
-                                else if ($region == 6)
-                                {
-                                 echo "<h5 class='mb-0'>La sede del evento sera en el IPN <strong> </strong></h5><br>";      
-                                 //echo "'<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.400951184565!2d-93.17515092795986!3d16.756715984026815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd964bb4d07fd%3A0x4e771d3242c2f25a!2sTecnol%C3%B3gico%20Nacional%20de%20M%C3%A9xico%20Campus%20Tuxtla%20Guti%C3%A9rrez!5e0!3m2!1ses-419!2smx!4v1688681077598!5m2!1ses-419!2smx' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>";
-                                }
-                              ?>
+      <h4><strong>La sede del evento sera en <?php echo $participante['dt_cede'] ?></strong></h4>                            
+
+      </div>
+       <div class="col-sm-8 col-md-8 offset-md-2 text-center">
+           <img class="img-fluid" src="img/agenda.png">
+      </div>
+
+      <div class="col-sm-12 col-md-12"><br><br>                            
+
+            <iframe src='<?php echo $participante['dt_ubicacion']; ?>' width='100%' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>                          
                               
-                              <img class="img-fluid" src="img/agenda.png">
+                            
         </div>
         <br><br>
 
--- update_registro.php
+++ update_registro.php
@@ -1,6 +1,11 @@
 <?php 
    include_once('databases_registro.php');
-   session_start();   
+   session_start(); 
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+   require 'PHPMailer/src/PHPMailer.php';
+   require 'PHPMailer/src/SMTP.php';
+   require 'PHPMailer/src/Exception.php';  
    if( $_POST )
    { 
    $apaterno = isset( $_POST['apaterno']) ? $_POST['apaterno'] : '';
@@ -24,7 +29,72 @@
    insert_registro($apaterno, $amaterno, $nombre, $email, $tel_ins, $ext, $tel_movil, $region, $entidad, $organizacion, $nom_org, $nom_org2, $cargo, $cargo2, $otro_cargo, $comentario);   
     $id_usuario =acces_registro($email);
     $id_user=$id_usuario['id_usuario'];
-    $_SESSION["id"]=$id_usuario['id_usuario'];  
+    $d_nombre=$id_usuario['dt_nombre']." ".$id_usuario['dt_apaterno']." ".$id_usuario['dt_amaterno'];
+    $d_email=$id_usuario['dt_email'];
+    $d_cede=$id_usuario['dt_cede'];
+    $d_fecha=$id_usuario['dt_fecha'];
+    $_SESSION["id"]=$id_usuario['id_usuario'];   
+    $body = file_get_contents('https://fese.mx');
+   //Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.fese.org.mx';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'inaes@fese.org.mx';                     //SMTP username
+    $mail->Password   = 'HeVr1043D';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have 
+    //Recipients
+    $mail->setFrom('inaes@fese.org.mx', 'FOROS DE VINCULACIÓN 2023.');
+    $mail->addAddress($d_email, $d_nombre);     //Add a recipient
+    //$mail->addAttachment('img/programa.png', 'new.jpg');    //Optional name
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    // Activo condificacción utf-8
+    $mail->CharSet = 'UTF-8';
+    $mail->Subject = 'FOROS DE VINCULACIÓN 2023..';
+    $mail->Body    = '  
+    <style>
+      h3 {color:blue; font-size:14px;}
+    </style>
+                  
+                                  
+               <table style="width:950px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">               
+                  <tr>
+                     <td align="center" style="padding:20px 0 30px 0;">
+                       <img src="http://forosdevinculacion.anuies.mx/img/logo_central.png" alt="" width="300" style="height:auto;display:block;" />
+                     </td>
+                  </tr>
+                  <tr>
+                    <td align="center" style="padding:10px 0 10px 0;">
+                    <h3>'.$d_nombre.' te damos la bienvenida a los <strong>FOROS DE VINCULACIÓN 2023</strong> que se llevaran a cabo en<br> '.$d_cede.' el '.$d_fecha.' </h3>
+                    </td>
+                  </tr>
+                  <tr>
+                  <td align="center" style="padding:0px 0 10px 0;">
+                    <h3><strong>Descarga los siguentes documentos</strong></h3>
+                     <h4>Marco de referencia del servicio social (Resultados de los Foros de Vinculación 2020)<a href="#">&nbsp;Descargar</a></h4>
+                     <h4>Marco de referencia del servicio social (Resultados de los Foros de Vinculación 2020)<a href="#">&nbsp;Descargar</a></h4>
+                  </td>
+                  </tr>
+                  <tr>
+                   <td align="center" style="padding:20px 0 30px 0;">
+                   <img src="http://forosdevinculacion.anuies.mx/img/Logos_Institucionales.png" alt="" width="600" style="height:auto;display:block;" />
+                   </td>                   
+                  </tr>  
+</table>
+
+                  <p>¡Nos vemos pronto!</p> ';
+    //$mail->AltBody = 'Gracias por leer';
+
+    $mail->send();
+} catch (Exception $e) {
+    echo "Mensaje de error: {$mail->ErrorInfo}";
+}
+
 ?>
     <script>
        window.location="datos.php";
@@ -32,6 +102,10 @@
 
 <?php
 }else{
+
+   $id_usuario =acces_registro($email);
+   $id_user=$id_usuario['id_usuario'];
+   $_SESSION["id"]=$id_usuario['id_usuario'];  
 //caso contario (else) es porque ese user ya esta registrado 
  ?>
 <script>
