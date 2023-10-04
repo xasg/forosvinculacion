1554ebdedb95d9525e47d69b5096b259a0f8f4ca
reivajfly <francisco@fese.mx>
2023-10-04 13:14:55 -0600

    se crea la rama generarConstancias2023

---
+++ contanciasForos2023/conec.php
@@ -0,0 +1,11 @@
+<?php
+/*$servername = "localhost";
+$dbname = "feseor6_foros2023";
+$username = "feseor6_foros2023";
+$password = "HeVr1043D";*/
+
+$servername = "localhost";
+$dbname = "forosvinculacion2023";
+$username = "root";
+$password = "";
+?>
---
+++ contanciasForos2023/insert_new.php
@@ -0,0 +1,19 @@
+<?php
+   include_once('databases_usuario.php');
+   mysqli_set_charset( $mysqli, 'utf8');
+   if( $_POST )
+   {     
+   $nombre = isset( $_POST['nombre']) ? $_POST['nombre'] : '';
+   $apaterno = isset( $_POST['apaterno']) ? $_POST['apaterno'] : '';
+   $amaterno = isset( $_POST['amaterno']) ? $_POST['amaterno'] : ''; 
+   $email = isset( $_POST['email']) ? $_POST['email'] : ''; 
+   $region = isset( $_POST['region']) ? $_POST['region'] : '';  
+   insert_user($nombre, $apaterno, $amaterno, $email, $region);
+
+?>
+    <script>
+       window.location="admin.php"
+    </script>
+<?php
+
+}
