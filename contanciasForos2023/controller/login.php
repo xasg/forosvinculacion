737e4813290b40afbcf38bd7a75aa5f9bed5ca49
reivajfly <francisco@fese.mx>
2023-10-12 13:54:29 -0600

    se realizaron cambios en la parte del admin

--- contanciasForos2023/controller/login.php
+++ contanciasForos2023/controller/login.php
@@ -1,24 +1,36 @@
 <?php
 require_once('databases_log.php');
  session_start();
- $correo = isset( $_POST['email']) ? $_POST['email'] : '';
+ $email = isset( $_POST['email']) ? $_POST['email'] : '';
  $region = isset( $_POST['region']) ? $_POST['region'] : '';
- $user =get_user_acces($correo, $region);
-if($user['dt_email']==$correo AND $user['dt_region']==$region AND $user['tp_usuario']==2)
+ $_SESSION['email'] = $email;
+ $_SESSION['region'] = $region;
+ $user =get_user_acces($email, $region);
+ 
+
+if($user == null)
  		{    		
- 		    $_SESSION["id"]=$user['id_usuario'];
+  		?>
+				<script>
+					//$_SESSION["id"]=$user['id_usuario'];
+ 		    		//$_SESSION["region"]=$user['dt_region'];
+					window.location="../UsuarioNoEncontrado.php"
+				</script>
+		<?php    
+ 		}elseif($user['dt_email']==$email AND $user['dt_region']==$region AND $user['tp_usuario']==2){
+			$_SESSION["id"]=$user['id_usuario'];
  		    $_SESSION["region"]=$user['dt_region'];
 
-  		?>
+		?>
 				<script>
 					window.location="../pdf/certificado.php"
 				</script>
-		<?php    
- 		}else{
-		?>
-				<script>
-					window.location="../../index.html"
-				</script>
 	   <?php    
  		  }
-       ?>
+       ?> 
+
+
+
+?>
+
+	   
