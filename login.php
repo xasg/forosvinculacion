92784243938152d04aa51481678b6cc67a197046
ANDRES ALEXIS MALFAVAUN <2203024384@alumnos.cua.uam.mx>
2024-03-06 14:40:18 -0600

    se cambia estilo de login y validaciones de login

--- login.php
+++ login.php
@@ -27,7 +27,42 @@
             field.value = field.value.toUpperCase() 
         }   
         </script>
+  <style>
+    .gradient-custom-2 {
+/* fallback for old browsers */
+background: #fccb90;
+/* background: #10312B; */
 
+/* Chrome 10-25, Safari 5.1-6 */
+/* background: -webkit-linear-gradient(120deg, #4f2550 10%, #57007B 30%, #8b45b4,#fafafa); */
+background: -webkit-linear-gradient(to top ,#10312B,#235b4e);
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+/* background: linear-gradient(120deg, #4f2550 10%, #57007B 30%, #8b45b4,#fafafa); */
+background: linear-gradient(to top ,#10312B,#235b4e 90%);
+}
+
+@media (min-width: 768px) {
+.gradient-form {
+height: 100vh !important;
+}
+}
+@media (min-width: 769px) {
+.gradient-custom-2 {
+border-top-right-radius: .3rem;
+border-bottom-right-radius: .3rem;
+}
+}
+.gradient-custom-2-border{
+   border:2px solid #235b4e;
+   box-shadow:1px 1px 6px #235b4e;
+   border-radius:12px;
+   transition:all linear .3s;
+}
+.gradient-custom-2-border:hover{
+   filter: drop-shadow(1px 1px 6px #235b4e);
+}
+  </style>
 
 </head>
 
@@ -71,7 +106,7 @@
   <br><br>
 
   <div class="container">
-    <form action="controller/login.php" method="POST">               
+   <!-- <form action="controller/login.php" method="POST">               
                 <div class="row">
                 <div class="col-md-12 text-center"><br><br>
                     <h5>Hola, ingresa tus datos de acceso.<br><br></h5>
@@ -112,9 +147,91 @@
 
                      </div>
                 </div>  
- </form>
+   </form>
     
- 
+  -->
+  <section class="h-100 gradient-form" style="background-color: #eee; min-height: 100vh;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-xl-10">
+            <div class="card rounded-3 text-black">
+              <div class="row g-0">
+                <div class="col-lg-6 gradient-custom-2-border">
+                  <div class="card-body p-md-5 mx-md-4 ">
+    
+                    <div class="text-center gradient-custom-2 p-4 mb-5">
+                      <!-- <img src="https://raw.githubusercontent.com/Katashikunomo/fempresarial/main/imgs/empresarial.png" -->
+                      <img src="img/logo_2024.png"
+                        style="width: 185px;" alt="logo">
+                      <br>
+                    </div>
+    
+                    <form action="controller/login.php" method="POST">
+                      <p>
+                        <b>Ingresa con tu correo y password</b>
+                      </p>
+    
+                      <div class="form-outline mb-4 ">
+                        <input type="email" id="correo" name="correo" class="form-control"
+                          placeholder="Ingresa tu correo" required/>
+                        <label class="form-label" for="form2Example11">Correo</label>
+                      </div>
+    
+                      <div class="form-outline mb-4">
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Ingresa tu password" required/>
+                        <label class="form-label" for="form2Example22">Password</label>
+                      </div>
+                      <div class="text-center pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" style="border-radius: 22px; border: 1px solid #4f2550; box-shadow:-1px 2px 5px #10312B;" type="submit">Iniciar sesión</button>                        <br>
+                        <!-- <a class="text-muted" href="#!">Recuperar password?</a> -->
+                      </div>
+                      <?php
+                     // Esto evaluará a TRUE así que el texto se imprimirá.
+                    if (isset($_GET['error'])) {
+                        ?>
+                        <div class="alert alert-danger" role="alert">
+                          Los datos de acceso que ingreso son incorrectos
+                        </div>                   
+
+                   <?php } ?>
+    
+                      <!-- <div class="d-flex align-items-center justify-content-center pb-4">
+                        <p class="mb-0 me-2">Don't have an account?</p>
+                        <button type="button" class="btn btn-outline-danger">Create new</button>
+                      </div> -->
+    
+                    </form>
+    
+                  </div>
+                </div>
+                <div class="row col-lg-6 d-flex align-items-center ">
+
+                  <div class="text-white ">
+                     <div class="col-md-12 text-center mt-5">
+                     <!-- <img src="img/icon.png" width="40%"alt=""> -->
+                     
+                     </div>
+                     <!-- <h4 class="mb-4">Panel de administración Foros de Vinculación 2024</h4> -->
+                     <img src="img/colab.png" width="100%" height="100%" alt="">
+                        <!-- <p class="small mb-0">
+                           Los Foros de Vinculacion 2024 para la Educación Dual, el Emprendimiento Asociativo, el Servicio Social Comunitario y la Industria de Alta Tecnología buscan dar seguimiento al trabajo colectivo
+                        </p>
+                        <p class="small mb-0">
+                           Y en particular fortalecer las acciones de vinculación con los sectores productivos estratégicos, que permita potencializar las líneas de acción impulsadas, con un enfoque primordialmente regional y estatal.
+                        </p> -->
+                        <!-- <img src="img/ANUIES_FESE.png" width="100%"alt=""> -->
+                  </div>
+                  <div class="col-md-12">
+                     <img src="img/Tira_de_Logos_IES.png" width="100%" height="100%" alt="">
+                  </div>
+                
+               </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </body>
 
